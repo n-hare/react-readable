0bf79a5fc35b769653c0f5f92126b9fe73f37123
Nathan Hare <n.hare@hotmail.com>
2017-08-18 20:21:49 -0400

    feat: Rearrange Redux infrastructure

--- readable-app/src/App.js
+++ readable-app/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+
+import Post from './components/Post'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>Readable</h1>
+        <h1>Readable</h1>
+        <Route  exact path='/' component={Post} />
+        <Route  exact path='/:category' render={({match})=>(<h1>Categories {match.params.category}</h1>)} />
+        <Route  exact path='/:category/:post_id' render={({match})=>(<h1>Categories {match.params.post_id}</h1>)} />
       </div>
     );
   }
