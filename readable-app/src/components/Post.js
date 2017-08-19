74de6c6111d2ca93f724674d32164f6aa45004b2
Nathan Hare <n.hare@hotmail.com>
2017-08-18 20:23:34 -0400

    feat: Add Post component

---
+++ readable-app/src/components/Post.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TiMessage, TiPencil, TiTag} from 'react-icons/lib/ti'
+
+const Post = (props) => {
+  return (
+    <article className='post__container'>
+     <div>
+        <h2>Title</h2>
+        <p>author</p>
+        <p>Body</p>
+      </div>
+      <ul className='post__controls'>
+        <li><TiMessage /> Comments</li>
+        <li><TiTag /> Category</li>
+        <li><TiPencil /> Edit</li>
+      </ul>
+    </article>
+  )
+}
+
+export default Post;
