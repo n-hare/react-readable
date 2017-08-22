9ef6a3021048b6a272bd733a22a4b1b7f5f1f2e5
Nathan Hare <n.hare@hotmail.com>
2017-08-22 01:15:34 -0400

    style: Pluralize reducers names

--- readable-app/src/reducers/root.js
+++ readable-app/src/reducers/root.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 import posts from './posts'
-import comments from './comment'
+import comments from './comments'
 
 const rootReducer = combineReducers({posts, comments})
 
